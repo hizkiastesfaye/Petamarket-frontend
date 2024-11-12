73f5cfa12a793d3953c78249abd2bf7a8e583cb7
hizkiastesfaye <tesfayehizkias@gmail.com>
2024-11-12 16:28:21 +0300

    frontend

--- petaMarket/src/features/User/pages/register.jsx
+++ petaMarket/src/features/User/pages/register.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react"
-
+import { Link } from "react-router-dom"
+import {useMutation, useQueryClient} from "@tanstack/react-query"
+import axios from 'axios'
 export default function Register(){
-    const [error,setError] = useState({})
+
+    // const queryClient = useQueryClient()
+    const [newFormError,setNewFormError] = useState({})
+    const [confirmPassword,setConfirmPassword]= useState('')
     const [formData, setFormData] = useState({
         firstname:'',
         lastname:'',
         country:'',
         tel:'',
-        email:'',
+        email:'',   
         password:'',
-        confirmPassword:'',
         role:'',
     })
     const ValidateError = ()=>{
@@ -26,9 +30,6 @@ export default function Register(){
         if(!formData.tel.trim()){
             formError.tel='tel is required'
         }
-        else if (!/^\d+$/.test(formData.phone)) {
-            formError.phone = 'Phone number must contain only digits';
-        }
         if(!formData.email.trim()){
             formError.email='email is required'
         }
@@ -44,52 +45,97 @@ export default function Register(){
         else if(formData.password.length <8){
             formError.password='password should be at least 8 characters'
         }
-        if (!formData.confirmPassword){
+        if (!confirmPassword){
             formError.confirmPassword='confirm password is required'
         }
-        else if(formData.password !== formData.confirmPassword){
+        else if(formData.password !== confirmPassword){
             formError.confirmPassword = 'confirm password is not match with password'
+            formData.password = '';
+            setConfirmPassword('')
         }
-        setError(formError)
-        return Object.keys((formError).length ===0)
+        setNewFormError(formError)
+        return {isValid:Object.keys(formError).length ===0, formError}
     }
 
     const handleChange=(e)=>{
         e.preventDefault();
         const {name,value} = e.target;
+        console.log(name,value)
+        if(name=='confirmPassword'){
+            setConfirmPassword(value)
+            console.log(name,value)
+        }
+        else{
         setFormData({...formData,[name]:value})
         console.log(formData)
+        }
+    }
+    const handleRadio=(e)=>{
+        const {name,value} = e.target;
+        setFormData({...formData,[name]:value})
+    }
+    const postMutuation = useMutation({
+        mutationFn:(newUser)=> axios.post('http://localhost:3004/user/register',newUser),
+        onSuccess:()=>{
+            console.log('successfully submitted')
+        },
+        onError:(error)=>{
+            console.log('Error', error)
+        }
+    })
+    const handleSubmit= async(event)=>{
+        event.preventDefault()
+        const {isValid,formError} = ValidateError()
+
+        if(!isValid){
+            console.log("Erroris: ",formError)
+        }
+        else {
+            console.log("This handleSubmit: ",formData)
+            postMutuation.mutate(formData)
+
+            setFormData({firstname:'',
+                lastname:'',
+                country:'',
+                tel:'',
+                email:'',   
+                password:'',
+                role:'',})
+                setConfirmPassword('')
+        }
     }
 
     return<>
-        <form className="max-w-2xl mx-auto my-20 rounded bg-white shadow-md">
+        <form onSubmit={handleSubmit} className="max-w-2xl mx-auto my-20 rounded bg-white shadow-md">
             <div className= "flex justify-center pb-5">
                 <h1 className="text-2xl font-extrabold text-gray-700 pt-10">SignUp</h1>
             </div>
-
-            <div className="mb-4 px-4 pt-3 flex justify-between items-center">
+            <div className="mb-1 px-4 pt-3 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Full Name: </label>
-                    <div className="flex justify-around w-3/4 gap-2">
-                        <input 
-                            type="text" 
-                            name="firstname" 
-                            placeholder="Your first name"
-                            value={formData.firstname}
-                            onChange={handleChange}
-                            className=" w-2/3 px-4 py-1 border rounded focus:outline-none focus:border-blue-500 mr"
-                        />
-                        <input 
-                            type="text" 
-                            name="lastname"  
-                            placeholder="Your last name"
-                            value={formData.lastname}
-                            onChange={handleChange}
-                            className="w-2/3 px-4 py-1 border rounded focus:outline-none focus:border-blue-500"
-                        /> 
-                    </div>
-
+                <div className="flex justify-around w-3/4 gap-2">
+                    <input 
+                        type="text" 
+                        name="firstname" 
+                        placeholder="Your first name"
+                        value={formData.firstname}
+                        onChange={handleChange}
+                        className=" w-2/3 px-4 py-1 border rounded focus:outline-none focus:border-blue-500 mr"
+                    />
+                    <input
+                        type="text" 
+                        name="lastname"  
+                        placeholder="Your last name"
+                        value={formData.lastname}
+                        onChange={handleChange}
+                        className="w-2/3 px-4 py-1 border rounded focus:outline-none focus:border-blue-500"
+                    /> 
+                </div>
             </div>
-            <div className="mb-4 px-4 flex justify-between items-center">
+            <div className="ml-[calc(30%)] text-red-500">
+                {newFormError.firstname && <p>** {newFormError.firstname}</p>}
+                {newFormError.lastname && <p>** {newFormError.lastname}</p>}
+            </div>
+            <div className="mb-1 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Country</label>
                 <input 
                     type="text" 
@@ -101,7 +147,8 @@ export default function Register(){
                 
                 />
             </div>
-            <div className=" mb-4 px-4 flex justify-between items-center">
+            {newFormError.country && <p className="ml-[calc(30%)] text-red-500">** {newFormError.country}</p>}
+            <div className=" mb-1 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">tel</label>
                 <input 
                     type="text" 
@@ -112,7 +159,8 @@ export default function Register(){
                     className="w-3/4 px-4 py-1 border rounded focus:outline-none focus:border-blue-500"
                 />
             </div>
-            <div className=" mb-4 px-4 flex justify-between items-center">
+            {newFormError.tel && <p className="ml-[calc(30%)] text-red-500">** {newFormError.tel}</p>}
+            <div className=" mb-1 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Email</label>
                 <input 
                     type="email" 
@@ -123,15 +171,17 @@ export default function Register(){
                     className="w-3/4 px-4 py-1 border rounded focus:outline-none focus:border-blue-500"
                     /> 
             </div>
-            <div className="mb-4 px-4 flex justify-between items-center">
+            {newFormError.email && <p className="ml-[calc(30%)] text-red-500">** {newFormError.email}</p>}
+            <div className="mb-1 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Role: </label>
                 <div className="w-3/4 px-4 py-1 flex justify-around items-start">
                     <div>
                         <input 
                                 type="radio" 
                                 id="buy"
-                                name="roleradio"
-                                value={formData.role}
+                                name="role"
+                                value= "Buyer"
+                                onChange={handleRadio}
                                 />
                         <label htmlFor="buy" className="text-sm">Buyer</label>
                     </div>
@@ -139,15 +189,16 @@ export default function Register(){
                         <input 
                                 type="radio" 
                                 id="sell"
-                                name="roleradio"
-                                value={formData.role}
-                                onChange={handleChange}
+                                name="role"
+                                value="Seller"
+                                onChange={handleRadio}
                                 />
                         <label htmlFor="sell" className="text-sm">Seller</label>
                     </div>
                 </div>
             </div>
-            <div className="mb-4 px-4 flex justify-between items-center">
+            {newFormError.role && <p className="ml-[calc(30%)] text-red-500">** {newFormError.role}</p>}
+            <div className="mb-1 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Password:</label>
                 <input 
                     type="password"
@@ -157,16 +208,23 @@ export default function Register(){
                     placeholder="Enter password with min 8 chars"
                     className="w-3/4 px-4 py-1 border rounded focus:outline-none focus:border-blue-500" /> 
             </div>
-            <div className="mb-4 px-4 pb-10 flex justify-between items-center">
+            {newFormError.password && <p className="ml-[calc(30%)] text-red-500">{newFormError.password}</p>}
+            <div className="mb-3 mt-4 px-4 flex justify-between items-center">
                 <label className="text-sm font-semibold text-gray-500">Confirm Password:</label>
                 <input 
                     type="password"
-                    name="password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleChange}
                     placeholder="confirm password"
                     className="w-3/4 px-4 py-1 border rounded focus:outline-none focus:border-blue-500" /> 
             </div>
+            <div className="pb-10 ">
+                {newFormError.confirmPassword && <p className="ml-[calc(30%)]  text-red-500">{newFormError.confirmPassword}</p>}
+            </div>
             <button type="submit" className="w-1/2 mx-auto flex justify-center  py-2 text-white rounded bg-blue-400 hover:bg-blue-600">SignUp</button>
             <br />
+            <p className="ml-[calc(50%)]">Already have an account? <Link to="/user/login" className="text-blue-600 underline">  Sign in</Link> </p>
         </form>
     </>
 }
